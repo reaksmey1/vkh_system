6be94fefccd5185f9d8248878d5181af7bfcddd0
reaksmsey1 <chea.reaksmey@gmail.com>
2015-02-09 16:06:09 +0700

    change database to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150126034636) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: true do |t|
     t.string   "plate_number"
     t.string   "name"
